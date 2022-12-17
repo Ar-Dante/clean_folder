4ab7c3989322c82b3c66cb19ed0c08f71e839557
Ar-Dante <atlas.monker@gmail.com>
2022-12-17 16:39:10 +0200

    change link

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='clean-folder',
       version='1',
       description='Script to sort files in a directory',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/Ar-Dante/clean_folder.git',
       author='Ar.Dante',
       author_email='abc',
       license='MIT',
